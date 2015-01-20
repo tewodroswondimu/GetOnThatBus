281a5d61a130c228a93fdf4e440289c1f94bc809
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-01-20 14:46:12 -0800

    Initial Commit

---
+++ GetOnThatBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetOnThatBus
+//
+//  Created by Tewodros Wondimu on 1/20/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
